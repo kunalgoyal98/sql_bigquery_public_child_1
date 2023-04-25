6d4232b43b08084b62f064fe708f71b933f82da4
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-25 16:28:22 +0000

    🔁 Modified     - Job        - REL_BQ_AIRFLOW

--- jobs/REL_BQ_AIRFLOW/code/tasks/DBT_0.py
+++ jobs/REL_BQ_AIRFLOW/code/tasks/DBT_0.py
@@ -3,12 +3,15 @@ def DBT_0():
     from datetime import timedelta
     from airflow.operators.bash import BashOperator
     envs = {}
+    envs["DBT_PARTIAL_PARSE"] = "true"
     envs["DBT_PROFILES_DIR"] = "/home/airflow/gcs/data"
+    envs["DBT_SEND_ANONYMOUS_USAGE_STATS"] = "false"
+    envs["DBT_FAIL_FAST"] = "true"
     envs["DBT_FULL_REFRESH"] = "true"
 
     return BashOperator(
         task_id = "DBT_0",
-        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_bigquery_public_child_1 --branch main --single-branch $tmpDir; cd $tmpDir/; dbt run --profile run_profile_bigquery; dbt test --profile run_profile_bigquery; ",
+        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_bigquery_public_child_1 --branch main --single-branch $tmpDir; cd $tmpDir/; dbt deps --profile run_profile_bigquery; dbt seed --profile run_profile_bigquery; dbt run --profile run_profile_bigquery; ",
         env = envs,
         append_env = True,
         **settings
