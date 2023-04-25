d462a10611b255c87a50d76b98f6404dd2e438c3
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-25 16:07:23 +0000

    ❌ Deleted     - Job        - REL_BQ_Databricks
    🔁 Modified
        - Job
           - REL_BQ_AIRFLOW

--- jobs/REL_BQ_AIRFLOW/code/tasks/DBT_0.py
+++ jobs/REL_BQ_AIRFLOW/code/tasks/DBT_0.py
@@ -3,11 +3,12 @@ def DBT_0():
     from datetime import timedelta
     from airflow.operators.bash import BashOperator
     envs = {}
+    envs["DBT_PROFILES_DIR"] = "/home/airflow/gcs/data"
     envs["DBT_FULL_REFRESH"] = "true"
 
     return BashOperator(
         task_id = "DBT_0",
-        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/test_repo_sql --branch main --single-branch $tmpDir; cd $tmpDir/test/SQL/uitesting/SQL_BigquerySharedBasic; dbt run; dbt test; ",
+        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_bigquery_public_child_1 --branch main --single-branch $tmpDir; cd $tmpDir/; dbt run --profile run_profile_bigquery; dbt test --profile run_profile_bigquery; ",
         env = envs,
         append_env = True,
         **settings
