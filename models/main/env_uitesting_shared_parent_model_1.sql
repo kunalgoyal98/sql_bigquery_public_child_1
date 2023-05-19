925ee1d61c46eacb2bcc5f12e2465a654ef2bcbc
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-05-19 06:23:14 +0000

    ❇️ New     - UnreferencedSource        - prophecy-qa.qa_test_dataset.ungrouped_customers
    🔁 Modified
        - Model
           - env_uitesting_shared_parent_model_1

--- models/main/env_uitesting_shared_parent_model_1.sql
+++ models/main/env_uitesting_shared_parent_model_1.sql
@@ -12,8 +12,44 @@ Reformat_1 AS (
   
   FROM all_type_table AS in0
 
+),
+
+prophecy_qa_qa_test_dataset_ungrouped_customers AS (
+
+  SELECT * 
+  
+  FROM prophecy-qa.qa_test_dataset.ungrouped_customers
+
+),
+
+Join_1 AS (
+
+  SELECT 
+    in0.c_int64 AS c_int64,
+    in0.c_bignumeric AS c_bignumeric,
+    in0.c_bool AS c_bool,
+    in0.c_bytes AS c_bytes,
+    in0.c_string AS c_string,
+    in0.c_float64 AS c_float64,
+    in0.c_numeric_1 AS c_numeric_1,
+    in0.c_numeric_2 AS c_numeric_2,
+    in0.c_date AS c_date,
+    in0.c_interval AS c_interval,
+    in0.c_time AS c_time,
+    in0.c_timestamp AS c_timestamp,
+    in0.c_datetime AS c_datetime,
+    in0.c_geography AS c_geography,
+    in0.c_json AS c_json,
+    in0.c_array_int64 AS c_array_int64,
+    in0.c_struct AS c_struct,
+    in0.p_date AS p_date
+  
+  FROM Reformat_1 AS in0
+  INNER JOIN prophecy_qa_qa_test_dataset_ungrouped_customers AS in1
+     ON in0.c_int64 != in1.id
+
 )
 
 SELECT *
 
-FROM Reformat_1
+FROM Join_1
