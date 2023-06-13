5de1d00360b2a4be9d82dbfbca14db0c1aa41415
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-06-13 09:24:17 +0000

    ❇️ New     - Job        - REL_BQ_DoAnything_Job        - REL_BQ_MWAA
    🔁 Modified
        - Job
           - REL_BQ_ProphecyManaged_Airflow

--- jobs/REL_BQ_DoAnything_Job/code/dag.py
+++ jobs/REL_BQ_DoAnything_Job/code/dag.py
@@ -5,36 +5,39 @@ from datetime import timedelta
 import airflow
 from airflow import DAG
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from tasks import DBT_0, DBT_0_1
+from tasks import DBT_0
 PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
 
 with DAG(
-    dag_id = "NPP4q9MIzskxZeICpwPZAw_", 
-    schedule_interval = "0 0 13 * *", 
+    dag_id = "uitesting_shared_team_SQL_ChildBigqueryShared_REL_BQ_DoAnything_Job", 
+    schedule_interval = "0 0 1 1 *", 
     default_args = {
       "owner": "Prophecy", 
+      "email": "abhisheks@prophecy.io,abhisheks@simpledatalabs.com", 
+      "email_on_failure": True, 
+      "email_on_retry": True, 
       "retry_exponential_backoff": True, 
       "retries": 1, 
-      "execution_timeout": timedelta(seconds = 900), 
+      "execution_timeout": timedelta(seconds = 1800), 
       "max_retry_delay": timedelta(minutes = 1.0), 
       "retry_delay": timedelta(minutes = 1.0), 
-      "trigger_rule": "all_success", 
-      "depends_on_past": False, 
+      "trigger_rule": "all_done", 
+      "depends_on_past": True, 
       "ignore_first_depends_on_past": True, 
       "wait_for_downstream": True, 
-      "priority_weight": 2, 
+      "priority_weight": 1, 
       "weight_rule": "downstream", 
-      "pool": "tB-MN8Ps", 
+      "pool": "testAutomationPool", 
+      "pool_slots": 2, 
+      "max_active_tis_per_dag": 1, 
       "do_xcom_push": True
     }, 
     start_date = pendulum.datetime(2023, 2, 28, tz = "UTC"), 
-    end_date = pendulum.datetime(2023, 7, 3, tz = "UTC"), 
+    end_date = pendulum.datetime(2080, 2, 28, tz = "UTC"), 
     catchup = True, 
-    dagrun_timeout = timedelta(seconds = 3600), 
-    max_active_runs = 25, 
+    dagrun_timeout = timedelta(seconds = 1800), 
+    max_active_runs = 26, 
     max_active_tasks = 2, 
-    tags = ["bigquery_shared"]
+    tags = ["doanything"]
 ) as dag:
     DBT_0_op = DBT_0()
-    DBT_0_1_op = DBT_0_1()
-    DBT_0_op >> DBT_0_1_op
