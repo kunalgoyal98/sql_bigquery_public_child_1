05c2c6c245f82e0c37f7d497b4e03abc85de0df4
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-06-21 16:45:39 +0000

    🔁 Modified     - Job        - REL_BQ_AIRFLOW        - REL_BQ_MWAA     - Model        - env_uitesting_shared_useall_model_1

--- jobs/REL_BQ_AIRFLOW/code/uitesting_shared_team_SQL_BigquerySharedBasic_REL_BQ_AIRFLOW/tasks/Script_1_1.py
+++
@@ -1,6 +0,0 @@
-def Script_1_1():
-    settings = {}
-    from datetime import timedelta
-    from airflow.operators.bash import BashOperator
-
-    return BashOperator(task_id = "Script_1_1", bash_command = "ls -ltr", **settings)
