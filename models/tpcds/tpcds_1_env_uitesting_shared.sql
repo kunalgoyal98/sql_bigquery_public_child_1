0bbd977bea027a366ff4864bd7e44ee13e36d0f0
kunalg@simpledatalabs.com <kunalg@simpledatalabs.com>
2023-11-21 05:36:21 +0000

    🔁 Modified     - Model        - env_uitesting_shared_useall_model_1        - tpcds_1_env_uitesting_shared     - Source        - prophecy-qa.qa_test_another_dataset        - prophecy-qa.dataset        - prophecy-qa.qa_suggestion_database        - prophecy-qa.qa_test_dataset     - Ungrouped Tables        - prophecy-qa.qa_test_dataset.ungrouped_customers

--- models/tpcds/tpcds_1_env_uitesting_shared.sql
+++ models/tpcds/tpcds_1_env_uitesting_shared.sql
@@ -41,9 +41,13 @@ SQLStatement_3 AS (
     sum(ss_ext_sales_price) AS itemrevenue,
     sum(ss_ext_sales_price) * 100 / sum(sum(ss_ext_sales_price)) OVER (PARTITION BY i_class) AS revenueratio
   
-  FROM qa_test_dataset.STORE_SALES, item, date_dim
+  FROM `prophecy-qa`.qa_test_dataset.STORE_SALES, item, date_dim
   
-  WHERE ss_item_sk = i_item_sk and i_category IN ('Women', 'Electronics', 'Shoes') and ss_sold_date_sk = d_date_sk and d_date BETWEEN CAST('2002-05-27' AS date) and DATE_ADD(DATE'2002-05-27', INTERVAL 30 DAY)
+  WHERE ss_item_sk = i_item_sk
+        and i_category IN ('Women', 'Electronics', 'Shoes')
+        and ss_sold_date_sk = d_date_sk
+        and d_date BETWEEN CAST('2002-05-27' AS date)
+        and DATE_ADD(DATE'2002-05-27', INTERVAL 30 DAY)
   
   GROUP BY 
     i_item_id, i_item_desc, i_category, i_class, i_current_price
@@ -79,24 +83,24 @@ SQLStatement_0 AS (
         THEN 1
       ELSE 0
     END) AS a_30_days,
-    sum(CASE
-      WHEN (cs_ship_date_sk - cs_sold_date_sk > 30) and 
-                       (cs_ship_date_sk - cs_sold_date_sk <= 60)
-        THEN 1
-      ELSE 0
-    END) AS a_31_60_days,
-    sum(CASE
-      WHEN (cs_ship_date_sk - cs_sold_date_sk > 60) and 
-                       (cs_ship_date_sk - cs_sold_date_sk <= 90)
-        THEN 1
-      ELSE 0
-    END) AS a_61_90_days,
-    sum(CASE
-      WHEN (cs_ship_date_sk - cs_sold_date_sk > 90) and
-                       (cs_ship_date_sk - cs_sold_date_sk <= 120)
-        THEN 1
-      ELSE 0
-    END) AS a_91_120_days,
+    sum(
+      CASE
+        WHEN (cs_ship_date_sk - cs_sold_date_sk > 30) and (cs_ship_date_sk - cs_sold_date_sk <= 60)
+          THEN 1
+        ELSE 0
+      END) AS a_31_60_days,
+    sum(
+      CASE
+        WHEN (cs_ship_date_sk - cs_sold_date_sk > 60) and (cs_ship_date_sk - cs_sold_date_sk <= 90)
+          THEN 1
+        ELSE 0
+      END) AS a_61_90_days,
+    sum(
+      CASE
+        WHEN (cs_ship_date_sk - cs_sold_date_sk > 90) and (cs_ship_date_sk - cs_sold_date_sk <= 120)
+          THEN 1
+        ELSE 0
+      END) AS a_91_120_days,
     sum(CASE
       WHEN (cs_ship_date_sk - cs_sold_date_sk > 120)
         THEN 1
@@ -105,7 +109,12 @@ SQLStatement_0 AS (
   
   FROM catalog_sales, warehouse, ship_mode, call_center, date_dim
   
-  WHERE d_month_seq BETWEEN 1200 and 1200 + 11 and cs_ship_date_sk = d_date_sk and cs_warehouse_sk = w_warehouse_sk and cs_ship_mode_sk = sm_ship_mode_sk and cs_call_center_sk = cc_call_center_sk
+  WHERE d_month_seq BETWEEN 1200
+        and 1200 + 11
+        and cs_ship_date_sk = d_date_sk
+        and cs_warehouse_sk = w_warehouse_sk
+        and cs_ship_mode_sk = sm_ship_mode_sk
+        and cs_call_center_sk = cc_call_center_sk
   
   GROUP BY 
     substr(w_warehouse_name, 1, 20), sm_type, cc_name
